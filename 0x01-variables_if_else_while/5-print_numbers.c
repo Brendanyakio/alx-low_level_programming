5c5b4bf29791c3104a98f6ff6f2a6bdc1ea1808b
Brendanyakio <nyakio75brenda@gmail.com>
2023-02-23 05:00:38 -0800

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - displaying numbers to the base of 10
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	return (0);
+}
