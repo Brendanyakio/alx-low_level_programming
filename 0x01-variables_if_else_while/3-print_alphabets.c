7dd176dae435391ce092bd48687207532174c982
Brendanyakio <nyakio75brenda@gmail.com>
2023-02-23 04:38:53 -0800

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Both capital and small letters
+ * Return: 0
+ */
+int main(void)
+{
+	char aplha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
