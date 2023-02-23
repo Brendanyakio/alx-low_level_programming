1fdb865855a6336052239dead31a8debd2eba42a
Brendanyakio <nyakio75brenda@gmail.com>
2023-02-23 05:19:36 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - other numbers but just use patchar dont use printf
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
